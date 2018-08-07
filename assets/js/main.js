b7c2ab75202777db6c87eb1670c943a7bf03a57a
Kris Koeckritz <kjk464@humboldt.edu>
2018-08-07 16:33:30 -0700

    darken project cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
 		
 	});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {
+		
+	});
+  });
