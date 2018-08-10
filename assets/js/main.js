e274464ed1d4374f31bf4e82eff14353ace47e8d
Kris Koeckritz <kjk464@humboldt.edu>
2018-08-09 18:41:44 -0700

    fix most mobile reponsive issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,5 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.tabs');
 	var instance = M.Tabs.init(elems, {
-		
 	});
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, {
-		
-	});
-  });
