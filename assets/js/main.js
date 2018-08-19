8efb601743474d2e647e79027abfec7b50d15782
Kris Koeckritz <kjk464@humboldt.edu>
2018-08-19 16:39:36 -0700

    remove swipe functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,5 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.tabs');
 	var instance = M.Tabs.init(elems, {
-		swipeable: true
 	});
 });
